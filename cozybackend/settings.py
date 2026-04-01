4cc00252a71dcc0e48333789658901692aff0e77
hacker777-cris <crispusgikonyo@gmail.com>
2026-04-01 19:14:52 +0300

    images for prod

--- cozybackend/settings.py
+++ cozybackend/settings.py
@@ -85,6 +85,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://cosycontent.com",
     "http://cosycontent.com",
     "https://api.cosycontent.com",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
