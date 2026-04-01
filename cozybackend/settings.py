90ebab3afd56d2ed7ff22f4378c72a89df37f580
hacker777-cris <crispusgikonyo@gmail.com>
2026-04-01 17:10:03 +0300

    images for prod

--- cozybackend/settings.py
+++ cozybackend/settings.py
@@ -145,6 +145,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Media files (user-uploaded files)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # --- Stripe Integration ---
 STRIPE_PUBLISHABLE_KEY = 'pk_test_placeholder'
