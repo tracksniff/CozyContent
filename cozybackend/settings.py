1dab3daf5e6c267d64e7cc820aeaf4239cd0c84d
hacker777-cris <crispusgikonyo@gmail.com>
2026-07-11 23:25:11 +0300

    changes

--- cozybackend/settings.py
+++ cozybackend/settings.py
@@ -98,7 +98,7 @@ ROOT_URLCONF = 'cozybackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
